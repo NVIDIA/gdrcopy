8987026bea1a6283d256d0f124856f93fc1793cd
Pak Markthub <pmarkthub@nvidia.com>
2019-06-06 15:02:44 -0700

    Checked for clock_gettime and added -lrt if necessary, and created config.h with autoconf

--- autogen.sh
+++ autogen.sh
@@ -21,6 +21,13 @@ then
     exit 1
 fi
 
+(set -x; autoheader)
+if [ $? -ne 0 ]
+then
+    echo "Error in autoheader!!"
+    exit 1
+fi
+
 (set -x; automake --add-missing --foreign)
 if [ $? -ne 0 ]
 then
