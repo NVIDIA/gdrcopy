eec92dccadd758166ec4a8ca63ca3f3c17dc72ea
Pak Markthub <pmarkthub@nvidia.com>
2019-07-08 16:32:04 -0700

    Modified the rpm-package build script to get version from include/gdrapi.h

--- packages/build-rpm-packages.sh
+++ packages/build-rpm-packages.sh
@@ -7,9 +7,14 @@ CWD=$(pwd)
 
 ex()
 {
-    if ! eval "$@"; then
-        echo "Failed to execute: $@" >&2
-        exit 1
+    local rc
+    echo "+ $@"
+    eval "$@"
+    rc=$?
+    
+    if [[ $rc -ne 0 ]]; then
+        echo "Failed with error $rc to execute: $@" >&2
+        exit $rc
     fi
 }
 
@@ -19,13 +24,12 @@ fi
 
 echo "Building rpm package ..."
 
-set -x
+ex cd ${SCRIPT_DIR_PATH}
 
-cd ${SCRIPT_DIR_PATH}
-
-VERSION=`grep Version: *.spec | cut -d : -f 2 | sed -e 's@\s@@g'`
-RELEASE=`grep "define _release" *.spec | cut -d" " -f"4"| sed -r -e 's/}//'`
-if [ "X$VERSION" == "X" ] || [ "X$RELEASE" == "X" ]; then
+MAJOR_VERSION=$(awk '/#define GDR_API_MAJOR_VERSION/ { print $3 }' ${TOP_DIR_PATH}/include/gdrapi.h | tr -d '\n')
+MINOR_VERSION=$(awk '/#define GDR_API_MINOR_VERSION/ { print $3 }' ${TOP_DIR_PATH}/include/gdrapi.h | tr -d '\n')
+VERSION="${MAJOR_VERSION}.${MINOR_VERSION}"
+if [ "X$VERSION" == "X" ]; then
     echo "Failed to get version numbers!" >&2
     exit 1
 fi
@@ -35,29 +39,36 @@ if [ ! -d "$tmpdir" ]; then
     echo "Failed to create a temp directory!" >&2
     exit 1
 fi
+
+echo "Building gdrcopy rpm packages version ${VERSION} ..."
+
 echo "Working in $tmpdir ..."
 
 #cp gdrcopy.spec ~/work/mellanox/rpmbuild/SPECS/
 
-cd ${TOP_DIR_PATH}
+ex cd ${TOP_DIR_PATH}
 
-mkdir -p $tmpdir/gdrcopy
-rm -rf $tmpdir/gdrcopy/*
-cp -r autogen.sh configure.ac packages/rhel/init.d insmod.sh Makefile.am README.md include src tests LICENSE packages/gdrcopy.spec $tmpdir/gdrcopy/
-rm -f $tmpdir/gdrcopy-$VERSION.tar.gz
-cd $tmpdir
-mv gdrcopy gdrcopy-$VERSION
-tar czvf gdrcopy-$VERSION.tar.gz gdrcopy-$VERSION
+ex mkdir -p $tmpdir/gdrcopy
+ex rm -rf $tmpdir/gdrcopy/*
+ex cp -r autogen.sh configure.ac packages/rhel/init.d insmod.sh Makefile.am README.md include src tests LICENSE packages/gdrcopy.spec $tmpdir/gdrcopy/
+ex rm -f $tmpdir/gdrcopy-$VERSION.tar.gz
 
-mkdir -p $tmpdir/topdir/{SRPMS,RPMS,SPECS,BUILD,SOURCES}
-cp gdrcopy-$VERSION/gdrcopy.spec $tmpdir/topdir/SPECS/
-cp gdrcopy-$VERSION.tar.gz $tmpdir/topdir/SOURCES/
+ex cd $tmpdir/gdrcopy
+ex find . -type f -exec sed -i "s/@VERSION@/${VERSION}/g" {} +
 
-rpmbuild -ba --nodeps --define "_topdir $tmpdir/topdir" --define 'dist %{nil}' --define "CUDA $CUDA"             --define "KVERSION $(uname -r)" $tmpdir/topdir/SPECS/gdrcopy.spec
+ex cd $tmpdir
+ex mv gdrcopy gdrcopy-$VERSION
+ex tar czvf gdrcopy-$VERSION.tar.gz gdrcopy-$VERSION
+
+ex mkdir -p $tmpdir/topdir/{SRPMS,RPMS,SPECS,BUILD,SOURCES}
+ex cp gdrcopy-$VERSION/gdrcopy.spec $tmpdir/topdir/SPECS/
+ex cp gdrcopy-$VERSION.tar.gz $tmpdir/topdir/SOURCES/
+
+rpmbuild -ba --nodeps --define "_topdir $tmpdir/topdir" --define 'dist %{nil}' --define "CUDA $CUDA" --define "KVERSION $(uname -r)" $tmpdir/topdir/SPECS/gdrcopy.spec
 rpms=`ls -1 $tmpdir/topdir/RPMS/*/*.rpm`
 srpm=`ls -1 $tmpdir/topdir/SRPMS/`
 echo $srpm $rpms
-cd ${CWD}
-mv $tmpdir/topdir/SRPMS/*.rpm .
-mv $tmpdir/topdir/RPMS/*/*.rpm .
+ex cd ${CWD}
+ex cp $tmpdir/topdir/SRPMS/*.rpm .
+ex cp $tmpdir/topdir/RPMS/*/*.rpm .
 
