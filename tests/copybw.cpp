eaad134efa42059160972f4bda86b4ffada30309
Pak Markthub <pmarkthub@nvidia.com>
2021-07-26 22:00:47 -0700

    Changed init_buf allocation in copybw to pinned memory

--- tests/copybw.cpp
+++ tests/copybw.cpp
@@ -63,7 +63,7 @@ void print_usage(const char *path)
 void run_test(CUdeviceptr d_A, size_t size)
 {
     uint32_t *init_buf = NULL;
-    init_buf = (uint32_t *)malloc(size);
+    ASSERTDRV(cuMemAllocHost((void **)&init_buf, size));
     ASSERT_NEQ(init_buf, (void*)0);
     init_hbuf_walking_bit(init_buf, size);
 
