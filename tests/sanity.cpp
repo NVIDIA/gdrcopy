7db8c7d0a7cfaefeb20a6849049b769550483b32
Davide Rossetti <drossetti@nvidia.com>
2024-11-07 02:07:00 +0000

    sanity: remove extra flushing flushing was needed before the fix for https://nvbugspro.nvidia.com/bug/4893052 [RM][GB180][BWL-1295] CUDA needs to switch from BAR0 to BAR1 doorbell note: to verify that, you need to run with the fix for the regression in https://nvbugspro.nvidia.com/bug/4939185 [Oberon-GB] cuda init fail

--- tests/sanity.cpp
+++ tests/sanity.cpp
@@ -552,24 +552,11 @@ void data_validation()
 
     uint32_t *buf_ptr = (uint32_t *)((char *)bar_ptr + off);
 
-#define wc_store_fence() do { asm volatile("DMB ishld"); } while(0)
-// flushing is needed to make the test pass
-//   but this alone is not enough
-#define bar1flush() do { READ_ONCE(buf_ptr[0]); } while(0)
-//   this is enough
-// #define bar1flush() do { READ_ONCE(buf_ptr[size/sizeof(uint32_t) - 2]); } while(0)
-//   this is no-op
-// #define bar1flush() do {  } while(0)
-
     print_dbg("check 1: MMIO CPU initialization + read back via cuMemcpy D->H\n");
     int errs = 0;
     int iters = 1000;
     for (int r = 0; r < iters; ++r) {
     init_hbuf_walking_bit(buf_ptr, size);
-    MB();
-    //wc_store_fence(); this is not enough
-    bar1flush();
-    MB();
     ASSERTDRV(cuMemcpyDtoH(copy_buf, d_ptr, size));
     errs += compare_buf(init_buf, copy_buf, size) ? 1 : 0;
     memset(copy_buf, 0xA5, size);
@@ -582,10 +569,6 @@ void data_validation()
     }
     print_dbg("check 2: gdr_copy_to_bar() + read back via cuMemcpy D->H\n");
     gdr_copy_to_mapping(mh, buf_ptr, init_buf, size);
-    MB();
-    //wc_store_fence();
-    bar1flush();
-    MB();
     ASSERTDRV(cuMemcpyDtoH(copy_buf, d_ptr, size));
     CHECK_EQ(compare_buf(init_buf, copy_buf, size), 0);
     memset(copy_buf, 0xA5, size);
